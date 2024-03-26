eba4cea3e741fa27053c28e8b277369a2cb148ae
loyyeeko <ko000029@algonquinlive.com>
2024-03-26 10:54:28 -0400

    setup for demo

--- auth/login.php
+++ auth/login.php
@@ -1,10 +1,30 @@
+<?php
 // a login page
 // youtube reference: https://www.youtube.com/watch?v=LC9GaXkdxF8
-<?php
-session_start();
+// session_start();
 
-if(isset($_SESSION['user_id'])) {
-    header("Location: index.php");
-} else {
-    header("Location: login.php");
-}
+// if(isset($_SESSION['user_id'])) {
+//     header("Location: index.php");
+// } else {
+//     header("Location: login.php");
+// }
+
+$array = array(
+    "name" => "david",
+    "age" => 23,
+    "company" => "killtasks"
+);
+?>
+
+<?php include("../partials/header.php") ?>
+    <form action="">
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+    </form>
+    <?php foreach($array as $key => $value) { ?>
+        <p><?= $key ?>: <?= $value ?></p>
+    <?php } ?>
+
+<?php include("../partials/footer.php") ?>
--- index.php
+++ index.php
@@ -5,10 +5,22 @@
   $_SESSION['user_id'] = 1;
   $_SESSION['user_name'] = "abc";
 
+  $array = array(
+    "name" => "david",
+    "age" => 23,
+    "company" => "killtasks"
+  );
+
+  $jsarr = json_encode($array);
 ?>
 <?php include("./partials/header.php") ?>
-  <?= session_id() ?> 
+  <?= var_dump($_SESSION) ?> 
+  <?= var_dump($jsarr) ?>
   <div>
-  <canvas id="myChart"></canvas>
+  <!-- <canvas id="myChart"></canvas> -->
 </div>
 <?php include("./partials/footer.php") ?>
+<script>
+  const jsdata = <?= $jsarr ?>;
+  // console.log(data);
+</script>
