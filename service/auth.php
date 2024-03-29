1f064cf0186f01f904312d5c99a2207e10268a5b
loyyeeko <ko000029@algonquinlive.com>
2024-03-28 23:01:23 -0400

    feat: login checker

--- service/auth.php
+++ service/auth.php
@@ -2,31 +2,41 @@
 
 declare(strict_types=1);
 include_once("../utils/checkers.php");
+class Response
+{
+  public bool $success;
+  public string $message;
 
+  public function __construct(bool $success, string $message)
+  {
+    $this->success = $success;
+    $this->message = $message;
+  }
+}
 
 class Auth
 {
-  static function login(PDO $conn): void
+
+  static function login(PDO $conn, string $email, string $password): Response
   {
-    $password = $_POST['password'];
-    $email = $_POST['email'];
+    session_start();
     $sql = "SELECT first_name, last_name, password FROM user WHERE email = '$email'";
-    $row = $conn->query($sql);
-    if (!empty($row)) {
-      foreach ($row as $r) {
-        if (!password_verify($password, $r['password'])) {
-          header("Location: ../auth/login.php");
-        } else {
-          $_SESSION['first_name'] = $r['first_name'];
-          $_SESSION['last_name'] = $r['last_name'];
-          $_SESSION['session_id'] = session_create_id($r['first_name'] .  $r['last_name']);
-          $_SESSION['email'] = $email;
-          header("Location: ../private/dashboard.php");
-        }
-      }
+    $stmt = $conn->query($sql);
+    $user = $stmt->fetch();
+    if (empty($user)) {
+      return new Response(false, "No user found");
+    } elseif (!password_verify($password, $user['password'])) {
+      return new Response(false, "Invalid email or password");
+    } else {
+      $_SESSION['first_name'] = $user['first_name'];
+      $_SESSION['last_name'] = $user['last_name'];
+      $_SESSION['session_id'] = session_create_id($user['first_name'] .  $user['last_name']);
+      $_SESSION['email'] = $email;
+      return new Response(true, "Welcome back! $user[first_name] $user[last_name]");
     }
   }
-  static function register(PDO $conn, string $first_name, string $last_name, string $email, string $password):array 
+
+  static function register(PDO $conn, string $first_name, string $last_name, string $email, string $password): array
   {
     try {
       $sql = 'INSERT INTO user (first_name, last_name, email, password) VALUES (:first_name, :last_name, :email, :password)';
@@ -36,14 +46,25 @@ class Auth
       $stmt->bindParam(':email', $email, PDO::PARAM_STR);
       $stmt->bindParam(":password", $password, PDO::PARAM_STR);
       $stmt->execute();
-      return array("success" => true, "message" => "User registered successfully");
+      return new Response(true, "User registered successfully");
     } catch (PDOException $e) {
-      return array("success" => false, "message" => reason($e->getMessage()));
+      return new Response(false, reason($e->getMessage()));
     }
   }
+  public static function logout()
+  {
+    session_start();
+    echo "You have been logged out";
+    echo "<br>Redirecting to login page...<br>";
+    session_destroy();
+    sleep(1);
+    header("Location: ../auth/login.php");
+  }
 }
-  function reason(string $err_msg) {
-    if(str_contains($err_msg, "Duplicate entry")) {
+
+  function reason(string $err_msg)
+  {
+    if (str_contains($err_msg, "Duplicate entry")) {
       return "Email already exists";
     }
   }
